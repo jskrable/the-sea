6fe2589e2de3e995d2b0c85954e8c989a4da7bbe
Skrable <jskrable@bu.edu>
2018-03-01 11:37:46 -0500

    predator hunt updates

--- src/Predator.java
+++ src/Predator.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class Predator extends Fish{
 
@@ -19,20 +20,42 @@ public class Predator extends Fish{
 	
 	// run method for predators
 	public void run(School s) {
-		hunt(s);
+		applyForce(hunt(s).mult(2.0f));
 		update();
 		borders();
 		render();
 	}
 	
 	// hunt method to aim for guppies
-	private void hunt(School s) {
-		ArrayList<Guppy> targets;
-		targets = s.guppies;
-		
-		for (Guppy g : targets) {
-			
+	private PVector hunt(School s) {
+		// pull in school of guppies
+		ArrayList<Guppy> guppies;
+		guppies = s.guppies;
+		// empty PVector to hold steer
+		PVector steer = new PVector();
+		PVector attack = new PVector();
+		float min = 100;
+		float d = 0;
+		// loop thru guppies to attack
+		for (Guppy g : guppies) {
+			d = PVector.dist(position, g.position);
+			if ((d > 0) && (d < min)) {
+				min = d;
+				attack = PVector.add(position, g.position);
+			}
+		}
+		attack.normalize();
+		attack.div(d);
+		steer.add(attack);
+		if (steer.mag() > 0) {
+			// steering = desired - velocity
+			steer.normalize();
+			steer.mult(maxspeed);
+			steer.sub(velocity);
+			steer.limit(maxforce);
+			return steer; 
+		} else {
+		return new PVector(0,0);
 		}
 	}
-	
 }
